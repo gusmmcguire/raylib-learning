14251a98d90e6f0c01fec571ef47aaafc8c6d666
Gus McGuire <gusmcg1856@gmail.com>
2023-05-10 13:59:54 -0600

    Simple jump

--- Dapper Dasher/Dapper Dasher.cpp
+++ Dapper Dasher/Dapper Dasher.cpp
@@ -1,21 +1,45 @@
 #include "raylib.h"
 
-int main()
-{
-    const int windowWidth = 512;
-    const int windowHeight = 380;
+int main() {
+	const int windowWidth = 512;
+	const int windowHeight = 380;
 
-    InitWindow(windowWidth, windowHeight, "Dapper Dasher");
-    SetTargetFPS(60);
+	//rect dimensions
+	const int width = 50;
+	const int height = 80;
 
-    while (!WindowShouldClose()) {
-        //start drawing
-        BeginDrawing();
-        ClearBackground(WHITE);
+	int posY = windowHeight - height;
+	int vel = 0;
 
-        //stop drawing
-        EndDrawing();
-    }
-    CloseWindow();
-    return 0;
+	//acceleration due to gravity (pixels/frame)/frame
+	const int gravity = 1;
+	const int jumpVel = -20;
+	bool isInAir = false;
+
+
+	InitWindow(windowWidth, windowHeight, "Dapper Dasher");
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose()) {
+		//start drawing
+		BeginDrawing();
+		ClearBackground(WHITE);
+		
+		if (isInAir) vel += gravity;
+		else vel = 0;
+
+		if (IsKeyPressed(KEY_SPACE) && !isInAir) vel = jumpVel;
+
+		//update position
+		posY += vel;
+		isInAir = posY <= windowHeight - height;
+
+
+		DrawRectangle(windowWidth / 2, posY, width, height, BLUE);
+
+		//stop drawing
+		EndDrawing();
+	}
+	CloseWindow();
+	return 0;
 }
