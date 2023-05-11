4c3b3776ae8b82b44a601054931fb822f537d940
Gus McGuire <gusmcg1856@gmail.com>
2023-05-10 22:16:57 -0600

    Dapper Dasher Complete
    Squashed commit of the following:
    commit ba98e1dfef9320156af6cd926f6dba885da96b20
    Author: Gus McGuire <gusmcg1856@gmail.com>
    Date:   Wed May 10 22:16:21 2023 -0600
    
        win lose conditions
    
    commit ba9ecfb22f074781bdbd65f05be871fb87d20cb9
    Author: Gus McGuire <gusmcg1856@gmail.com>
    Date:   Wed May 10 22:12:04 2023 -0600
    
        collision detection
    
    commit 44c4d01a57be345b04ebadce4b145b2cf6239c16
    Author: Gus McGuire <gusmcg1856@gmail.com>
    Date:   Wed May 10 21:55:24 2023 -0600
    
        paralax background
    
    commit 18f967e61f0e99e417987ba376e8e7d991cb6b95
    Author: Gus McGuire <gusmcg1856@gmail.com>
    Date:   Wed May 10 21:37:09 2023 -0600
    
        refactor to be more DRY
    
    commit 1ad425e65fb7f162949e12d61f092300045bac04
    Author: Gus McGuire <gusmcg1856@gmail.com>
    Date:   Wed May 10 21:20:35 2023 -0600
    
        loop for expandable hazard count refactor
    
    commit bcb4d1ea42823dfe17ace9c05533d3cca1fb3dad
    Author: Gus McGuire <gusmcg1856@gmail.com>
    Date:   Wed May 10 20:50:07 2023 -0600
    
        Animated hazards and animation struct
    
    commit bc5d0fc5f68d82588835c01f3d369327be21c0f4
    Author: Gus McGuire <gusmcg1856@gmail.com>
    Date:   Wed May 10 20:03:02 2023 -0600
    
        animated character
    
    commit 58f8f51a5e168d1c135d0e4168ea65d8a24cc0bb
    Author: Gus McGuire <gusmcg1856@gmail.com>
    Date:   Wed May 10 14:29:16 2023 -0600
    
        using sprite and delta time
    
    commit 14251a98d90e6f0c01fec571ef47aaafc8c6d666
    Author: Gus McGuire <gusmcg1856@gmail.com>
    Date:   Wed May 10 13:59:54 2023 -0600
    
        Simple jump
    
    commit be6bc512fe3545eea5b4f5da0f283752271678b0
    Author: Gus McGuire <gusmcg1856@gmail.com>
    Date:   Wed May 10 13:34:46 2023 -0600
    
        Initial setup

---
+++ Dapper Dasher/Dapper Dasher.cpp
@@ -0,0 +1,188 @@
+#include "raylib.h"
+
+struct AnimData {
+	Rectangle rec;
+	Vector2 pos;
+	int frame;
+	float updateTime;
+	float runningTime;
+	float maxFrame;
+};
+
+
+bool isOnGround(const AnimData& data, const int& windowHeight) {
+	return data.pos.y >= windowHeight - data.rec.height;
+}
+
+void updateAnimData(AnimData& data, const float& deltaTime) {
+	data.runningTime += deltaTime;
+	if (data.runningTime >= data.updateTime) {
+		data.runningTime = 0;
+		data.rec.x = data.frame * data.rec.width;
+		if (++data.frame > data.maxFrame) data.frame = 0;
+	}
+}
+
+void drawBackground(Vector2& bg1Pos, Vector2& bg2Pos, Vector2& mg1Pos, Vector2& mg2Pos, Vector2& fg1Pos, Vector2& fg2Pos, const Texture2D& background, const Texture2D& midground, const Texture2D& foreground, const float dt) {
+	bg1Pos.x -= 20 * dt;
+	mg1Pos.x -= 40 * dt;
+	fg1Pos.x -= 80 * dt;
+	if (bg1Pos.x <= -background.width * 2) {
+		bg1Pos.x = 0.0;
+	}
+	if (mg1Pos.x <= -midground.width * 2) {
+		mg1Pos.x = 0.0;
+	}
+	if (fg1Pos.x <= -foreground.width * 2) {
+		fg1Pos.x = 0.0;
+	}
+	//background
+	DrawTextureEx(background, bg1Pos, 0.0, 2.0, WHITE);
+	bg2Pos.x = bg1Pos.x + background.width * 2;
+	DrawTextureEx(background, bg2Pos, 0.0, 2.0, WHITE);
+	//midground
+	DrawTextureEx(midground, mg1Pos, 0.0, 2.0, WHITE);
+	mg2Pos.x = mg1Pos.x + midground.width * 2;
+	DrawTextureEx(midground, mg2Pos, 0.0, 2.0, WHITE);
+	//foreground
+	DrawTextureEx(foreground, fg1Pos, 0.0, 2.0, WHITE);
+	fg2Pos.x = fg1Pos.x + foreground.width * 2;
+	DrawTextureEx(foreground, fg2Pos, 0.0, 2.0, WHITE);
+}
+
+void handlePlayer(int& curVel, const int& gravity, const float& dt, const int& jumpVel, AnimData& scarfyData, const int& windowHeight, const Texture2D& scarfy) {
+	bool isInAir = !isOnGround(scarfyData, windowHeight);
+	if (isInAir) curVel += gravity * dt;
+	else curVel = 0;
+	if (IsKeyPressed(KEY_SPACE) && !isInAir) curVel = jumpVel;
+
+	scarfyData.pos.y += curVel * dt;
+
+	if (!isInAir) updateAnimData(scarfyData, dt);
+	DrawTextureRec(scarfy, scarfyData.rec, scarfyData.pos, WHITE);
+}
+
+void handleNebulae(const int& sizeOfNebulae, AnimData nebulae[6], int nebVel, const float& dt, const Texture2D& nebula) {
+	for (int i = 0; i < sizeOfNebulae; i++) {
+		nebulae[i].pos.x += nebVel * dt;
+
+		updateAnimData(nebulae[i], dt);
+		DrawTextureRec(nebula, nebulae[i].rec, nebulae[i].pos, WHITE);
+	}
+}
+
+int main() {
+	const int windowDimensions[]{ 512, 380 };
+	InitWindow(windowDimensions[0], windowDimensions[1], "Dapper Dasher");
+
+	//NEBULA VARIABLES
+	int nebVel = -200;
+	Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
+	const int sizeOfNebulae{ 6 };
+	AnimData nebulae[sizeOfNebulae]{};
+	for (int i = 0; i < sizeOfNebulae; i++) {
+		nebulae[i].rec.x = 0.0;
+		nebulae[i].rec.y = 0.0;
+		nebulae[i].rec.width = nebula.width / 8;
+		nebulae[i].rec.height = nebula.height / 8;
+
+		nebulae[i].pos.y = windowDimensions[1] - nebula.height / 8;
+
+		nebulae[i].frame = 0;
+		nebulae[i].maxFrame = 7;
+
+		nebulae[i].runningTime = 0.0;
+		nebulae[i].updateTime = 1.0 / 16.0;
+		nebulae[i].pos.x = windowDimensions[0] + (300 * i);
+	}
+
+	float finishLine{ nebulae[sizeOfNebulae - 1].pos.x  + 100};
+
+	//SCARFY VARIABLES
+	Texture2D scarfy = LoadTexture("textures/scarfy.png");
+	AnimData scarfyData{
+		{ 0.0, 0.0, scarfy.width / 6, scarfy.height },
+		{ windowDimensions[0] / 2 - scarfyData.rec.width / 2, windowDimensions[1] - scarfyData.rec.height },
+		0,
+		1.0 / 10.0,
+		0,
+		5
+	};
+
+	const int gravity = 1000;
+	int curVel = 0;
+	const int jumpVel = -600;
+	bool isInAir = false;
+	bool collision{};
+
+	//BACKGROUND VARIABLES
+	Texture2D background = LoadTexture("textures/far-buildings.png");
+	Vector2 bg1Pos{ 0.0 , 0.0 };
+	Vector2 bg2Pos{ background.width * 2 , 0.0 };
+
+	//MIDGROUND VARIABLES
+	Texture2D midground = LoadTexture("textures/back-buildings.png");
+	Vector2 mg1Pos{ 0.0 , 0.0 };
+	Vector2 mg2Pos{ midground.width * 2 , 0.0 };
+
+	//FOREGROUND VARIABLES
+	Texture2D foreground = LoadTexture("textures/foreground.png");
+	Vector2 fg1Pos{ 0.0 , 0.0 };
+	Vector2 fg2Pos{ foreground.width * 2 , 0.0 };
+
+
+	SetTargetFPS(60);
+	while (!WindowShouldClose()) {
+		const float dt = GetFrameTime();
+
+		BeginDrawing();
+		ClearBackground(WHITE);
+
+		drawBackground(bg1Pos, bg2Pos, mg1Pos, mg2Pos, fg1Pos, fg2Pos, background, midground, foreground, dt);
+
+		for (const AnimData& nebula : nebulae) {
+			float pad{ 40 };
+			Rectangle nebRec{
+				nebula.pos.x + pad,
+				nebula.pos.y + pad,
+				nebula.rec.width - 2 * pad,
+				nebula.rec.height - 2 * pad
+			};
+			Rectangle scarfyRec{
+				scarfyData.pos.x,
+				scarfyData.pos.y,
+				scarfyData.rec.width,
+				scarfyData.rec.height
+			};
+			collision = CheckCollisionRecs(nebRec, scarfyRec);
+			if (collision) break;
+		}
+
+		if (collision) {
+			DrawText("Game Over!", 200, 200, 40, RED);
+		}
+		else if (finishLine <= scarfyData.pos.x) {
+			DrawText("You Win!", 200, 200, 40, GREEN);
+		}
+		else {
+			handleNebulae(sizeOfNebulae, nebulae, nebVel, dt, nebula);
+			handlePlayer(curVel, gravity, dt, jumpVel, scarfyData, windowDimensions[1], scarfy);
+			finishLine += nebVel * dt;
+		}
+
+
+		//stop drawing
+		EndDrawing();
+	}
+
+	UnloadTexture(scarfy);
+	UnloadTexture(nebula);
+	UnloadTexture(background);
+	UnloadTexture(midground);
+	UnloadTexture(foreground);
+
+	CloseWindow();
+	return 0;
+}
+
+
